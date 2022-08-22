0a235da9f45df5026b44bc32d08f822fd837bedb
Ashley Shookhye <ashley.shookhye@gmail.com>
2022-08-22 21:41:09 +0400

    to v3

--- src/main/java/com/example/springdemo/SpringDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -34,7 +34,7 @@ class HomeController {
 
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello World v2";
+        return "Hello World v3";
     }
 
     @GetMapping("/products")
